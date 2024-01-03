6467add21d89cbcc21e5516abe83053ce6834f1c
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2024-01-03 14:33:18 +0600

    delete a user's watchlist & delete api naming improved

--- index.js
+++ index.js
@@ -62,11 +62,20 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/watchlist/:id", async (req, res) => {
+    app.delete("/watchlist/id/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
 
       const result = await watchListCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Delete a user's full watchlist
+    app.delete("/watchlist/email/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+
+      const result = await watchListCollection.deleteMany(query);
       res.send(result);
     });
 
